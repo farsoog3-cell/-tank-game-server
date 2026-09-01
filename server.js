db3ea5b850a6b7259e84e023cf6db4225e56f600
farsoog3-cell <farsoog3@gmail.com>
2026-09-01 22:28:39 +0300

    Refactor socket event handling and improve room management

--- server.js
+++ server.js
@@ -11,117 +11,126 @@ const io = new Server(server, {
 let rooms = {};
 
 io.on('connection', (socket) => {
-    console.log(`مستخدم متصل: ${socket.id}`);
-    socket.emit('set_my_id', { id: socket.id });
+    socket.emit("set_my_id", { id: socket.id });
 
-    socket.on('get_rooms', () => {
-        socket.emit('rooms_list', getRoomsArray());
+    socket.on("get_rooms", () => {
+        let roomsList = [];
+        for (let roomId in rooms) {
+            roomsList.push({
+                id: roomId,
+                name: rooms[roomId].name,
+                bet: rooms[roomId].bet,
+                hostFlag: rooms[roomId].hostFlag,
+                guestJoined: rooms[roomId].guestJoined
+            });
+        }
+        socket.emit("rooms_list", roomsList);
     });
 
-    socket.on('create_room', (data) => {
+    socket.on("create_room", (data) => {
         let roomId = 'room_' + Math.random().toString(36).substring(2, 7);
         rooms[roomId] = {
             id: roomId,
             name: data.name,
             bet: data.bet,
-            hostId: socket.id,
-            hostFlag: data.hostFlag,
-            hostReady: false,
-            guestId: null,
+            hostFlag: data.hostFlag || 'green',
             guestFlag: data.hostFlag === 'green' ? 'red' : 'green',
-            guestReady: false
+            hostId: socket.id,
+            guestId: null,
+            hostReady: false,
+            guestReady: false,
+            guestJoined: false
         };
         socket.join(roomId);
-        socket.emit('room_joined', { 
-            id: roomId, name: data.name, bet: data.bet, isHost: true, 
-            hostFlag: data.hostFlag, guestFlag: rooms[roomId].guestFlag 
-        });
-        io.emit('rooms_list', getRoomsArray());
+        socket.emit("room_joined", { id: roomId, isHost: true, ...rooms[roomId] });
+        io.emit("rooms_list", getPublicRooms());
     });
 
-    socket.on('join_room', (data) => {
+    socket.on("join_room", (data) => {
         let room = rooms[data.roomId];
-        if (room && !room.guestId) {
+        if (room && !room.guestJoined) {
             room.guestId = socket.id;
-            socket.join(room.id);
-            socket.emit('room_joined', { 
-                id: room.id, name: room.name, bet: room.bet, isHost: false, 
-                hostFlag: room.hostFlag, guestFlag: room.guestFlag 
-            });
-            io.to(room.hostId).emit('room_update', { guestJoined: true, guestFlag: room.guestFlag });
-            io.emit('rooms_list', getRoomsArray());
+            room.guestJoined = true;
+            socket.join(data.roomId);
+            
+            socket.emit("room_joined", { id: data.roomId, isHost: false, ...room });
+            io.to(room.hostId).emit("room_update", { guestJoined: true });
+            io.emit("rooms_list", getPublicRooms());
         }
     });
 
-    socket.on('set_ready', (data) => {
+    socket.on("set_ready", (data) => {
         let room = rooms[data.roomId];
         if (room) {
-            if (room.hostId === socket.id) room.hostReady = data.ready;
-            if (room.guestId === socket.id) room.guestReady = data.ready;
+            if (socket.id === room.hostId) room.hostReady = data.ready;
+            else if (socket.id === room.guestId) room.guestReady = data.ready;
 
-            io.to(room.id).emit('room_ready_update', {
+            io.to(data.roomId).emit("room_ready_update", {
                 hostReady: room.hostReady,
                 guestReady: room.guestReady
             });
         }
     });
 
-    socket.on('start_game', (data) => {
+    socket.on("start_game", (data) => {
         let room = rooms[data.roomId];
-        if (room && room.hostId === socket.id && room.hostReady && room.guestReady) {
-            io.to(room.id).emit('start_game');
+        if (room && socket.id === room.hostId && room.hostReady && room.guestReady) {
+            io.to(data.roomId).emit("start_game");
         }
     });
 
-    // نظام نقل إحداثيات وأحداث الحرب والقتال بين الطرفين
-    socket.on('tank_move', (data) => {
-        socket.broadcast.to(data.roomId).emit('remote_tank_move', data);
+    socket.on("spawn_tank", (data) => {
+        socket.to(data.roomId).emit("remote_tank_spawn", { x: data.x, z: data.z, type: data.type });
     });
 
-    socket.on('spawn_tank', (data) => {
-        socket.broadcast.to(data.roomId).emit('remote_tank_spawn', data);
+    socket.on("tank_move", (data) => {
+        socket.to(data.roomId).emit("remote_tank_move", data);
     });
 
-    socket.on('shoot', (data) => {
-        socket.broadcast.to(data.roomId).emit('remote_shoot', data);
+    socket.on("shoot", (data) => {
+        socket.to(data.roomId).emit("remote_shoot", data);
     });
 
-    socket.on('tank_damaged', (data) => {
-        socket.broadcast.to(data.roomId).emit('remote_tank_damaged', data);
+    socket.on("tank_damaged", (data) => {
+        socket.to(data.roomId).emit("remote_tank_damaged", data);
     });
 
-    socket.on('capture_rig', (data) => {
-        socket.broadcast.to(data.roomId).emit('rig_captured', data);
+    socket.on("capture_rig", (data) => {
+        socket.to(data.roomId).emit("rig_captured", data);
     });
 
-    socket.on('leave_room', (data) => {
-        if (rooms[data.roomId]) {
-            delete rooms[data.roomId];
-            io.emit('rooms_list', getRoomsArray());
-        }
-        socket.leave(data.roomId);
+    socket.on("leave_room", (data) => {
+        handleUserLeave(socket);
     });
 
-    socket.on('disconnect', () => {
-        for (let rId in rooms) {
-            if (rooms[rId].hostId === socket.id || rooms[rId].guestId === socket.id) {
-                io.to(rId).emit('room_update', { guestJoined: false });
-                delete rooms[rId];
-            }
-        }
-        io.emit('rooms_list', getRoomsArray());
+    socket.on("disconnect", () => {
+        handleUserLeave(socket);
     });
 });
 
-function getRoomsArray() {
-    return Object.keys(rooms).map(id => ({
-        id: id,
-        name: rooms[id].name,
-        bet: rooms[id].bet,
-        hostFlag: rooms[id].hostFlag,
-        guestJoined: rooms[id].guestId !== null
-    }));
+function handleUserLeave(socket) {
+    for (let roomId in rooms) {
+        let room = rooms[roomId];
+        if (room.hostId === socket.id || room.guestId === socket.id) {
+            socket.to(roomId).emit("room_update", { guestJoined: false });
+            delete rooms[roomId];
+            io.emit("rooms_list", getPublicRooms());
+            break;
+        }
+    }
 }
 
-const PORT = process.env.PORT || 10000;
-server.listen(PORT, () => console.log(`السيرفر يعمل على المنفذ ${PORT}`));
+function getPublicRooms() {
+    let list = [];
+    for (let id in rooms) {
+        if (!rooms[id].guestJoined) {
+            list.push({ id, name: rooms[id].name, bet: rooms[id].bet, hostFlag: rooms[id].hostFlag });
+        }
+    }
+    return list;
+}
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
